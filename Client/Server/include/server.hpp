3725dad8075ffa7b9e2f159793d6385504cf8233
Oxym <atidote63240@gmail.com>
2013-10-22 22:12:57 +0200

    fuck

---
+++ Client/Server/include/server.hpp
@@ -0,0 +1,34 @@
+#ifndef SERVER
+# define SERVER
+
+#include <list>
+#include <iostream>
+
+#define WELCOME_MSG "salut"
+
+using namespace std;
+
+class Server
+{
+public:
+/*	typedef	list<Player*>	PlayerList; // Creation de la liste des joueurs
+	typedef	list<Game*>		GameList;   // Liste des parties.
+	*/
+
+	Server(int port = SERVER_PORT);
+	~Server();
+	void		run();
+	void		stop();
+	/*Player*     findPlayer(string& ip);
+	PlayerList& getPlayers();
+	Game*&      getCurrentGame();*/
+
+private:
+	/*PlayerList	players;
+	GameList	games;
+	ISocket		*socket;
+	Game*   	game;*/
+	bool		running;
+};
+
+#endif // SERVER
--- Client/Server/src/CPP/main_server.cpp
+++ Client/Server/src/CPP/main_server.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-#define BUFFSIZE 1024
+#include "include/server.hpp"
 
 using namespace std;
 
-int	main() 
-{
+int	main() {
     srand(43);
 
-    try 
+/*  try 
 	{
-		std::cout << "Test Connection Server" << std::endl;
     } 
 	catch (std::exception* e)
 	{
         std::cerr << e->what() << std::endl;
         delete e;
-    }
+    }*/
     return 0;
 }
