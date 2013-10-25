ef9a69c963ae04366f565bcc66067cbf92d99f8d
coincoincoin <benjamin.desbois@epitech.eu>
2013-10-25 15:44:40 +0200

    logger dans le client

--- Client/Client/src/CPP/main_client.cpp
+++ Client/Client/src/CPP/main_client.cpp
@@ -1,4 +1,8 @@
+#include "log.hpp"
+
 int		main()
-{
+{	Logger::Instance()->open("LOG.txt");
+	Logger::Instance()->log(-1, "----------------Client Initialized-------------\n");
+	Logger::Instance()->log(-1, "Exit with code 0");
 	return (0);
 }
