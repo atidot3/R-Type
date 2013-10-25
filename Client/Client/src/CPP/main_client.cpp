1c20f3fc8f7c1edc3341af5d7afe59fc5a5926f2
coincoincoin <benjamin.desbois@epitech.eu>
2013-10-25 15:54:14 +0200

    maj log

--- Client/Client/src/CPP/main_client.cpp
+++ Client/Client/src/CPP/main_client.cpp
@@ -1,8 +1,8 @@
 #include "log.hpp"
 
 int		main()
-{	Logger::Instance()->open("LOG.txt");
-	Logger::Instance()->log(-1, "----------------Client Initialized-------------\n");
-	Logger::Instance()->log(-1, "Exit with code 0");
+{	Logger::Instance()->open("COIN.txt");
+	Logger::Instance()->log(3, "----------------Client Initialized-------------\n");
+	Logger::Instance()->log(3, "Exit with code 0");
 	return (0);
 }
