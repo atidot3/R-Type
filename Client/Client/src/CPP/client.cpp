58b162e424aae8b2e886d1f96b7770bd316ba585
coincoincoin <benjamin.desbois@epitech.eu>
2013-10-28 14:32:00 +0100

    maj client
    (/!\peut etre boom /!\)

---
+++ Client/Client/src/CPP/client.cpp
@@ -0,0 +1,80 @@
+#include "client.hpp"
+#include "time.h"
+#include "stdlib.h"
+
+Client::Client(int port) : running(false)
+{
+	socket = new AbstractSocket(port);
+}
+
+Client::~Client()
+{
+/*	while (games.size() > 0)
+    {
+        delete games.front();
+        games.pop_front();
+    }
+	delete socket; */
+}
+
+void		Client::stop()
+{
+    running = false;
+}
+/*
+ Client::PlayerList& Client::getPlayers()
+{
+    return players;
+}
+
+Game*&      Client::getCurrentGame()
+{
+    return game;
+}
+
+Player*     findPlayer(string& ip)
+{
+    return (NULL);
+} */
+
+void		Client::run()
+{
+	Logger::Instance()->open("Client.txt");
+	string	buff, ip;
+//    Player* p;
+
+    srand(time(NULL));
+	cout << "\t\t\t\tR-Type Started..." << endl << endl << endl << "Please wait...." << endl;
+    try
+	{
+		running = true;
+		while (running == true)
+		{
+			buff = socket->recv(100, &ip);
+		    /*if ((p = findPlayer(ip)))
+		    {
+		        p->recv(buff);
+		    }*/
+		    if (buff == WELCOME_MSG)
+		    {
+		        cout << "New client " << ip << endl;
+	/*	        if (!game)
+		            game = new Game(this);
+		        if (game->started() || game->getPlayers().size() >= MAX_PLAYERS)
+		        {
+		            // New game
+		            game = new Game(this);
+		        }
+		        cout << "Added to game " << game->getId() << endl;
+		        p = new Player(game, ip);
+		        players.push_back(p); */
+		    }
+		}
+	}
+	catch (std::exception *e)
+	{
+        cerr << e->what() << endl;
+        delete e;
+        Sleep(20000);
+    }
+}
--- Client/Client/src/CPP/main_client.cpp
+++ Client/Client/src/CPP/main_client.cpp
@@ -1,9 +1,38 @@
+#include <iostream>
+#include "client.hpp"
+#include <stdlib.h>
+#include <vector>
 #include "log.hpp"
 
-int		main()
-{	
-	Logger::Instance()->open("COIN.txt");
-	Logger::Instance()->log(3, "----------------Client Initialized-------------\n");
-	Logger::Instance()->log(3, "Exit with code 0");
-	return (0);
+using namespace std;
+
+int	main()
+{
+	Logger::Instance()->open("Client.txt");
+	Logger::Instance()->log(0, "----------------Client Initialized-------------\n");
+	srand(43);
+	try 
+	{
+		Client	clie;
+		clie.run();
+    } 
+	catch (std::exception* e)
+	{
+        std::cerr << e->what() << std::endl;
+		Logger::Instance()->log(3, e->what());
+        delete e;
+    }
+	Logger::Instance()->log(0, "Exit with code 0");
+    return 0;
 }
+
+//#include "log.hpp"
+
+//int		main()
+//{
+//	Logger::Instance()->open("COIN.txt");
+//	Logger::Instance()->log(3, "----------------Client Initialized-------------\n");
+//	Logger::Instance()->log(3, strcat(prout, "YOUHOU"));
+//	Logger::Instance()->log(3, "Exit with code 0");
+//	return (0);
+//}
